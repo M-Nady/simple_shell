e9e843ba364d2479a3e014b721297dc773803701
m-nady <mo7md.nady@gmail.com>
2023-10-10 06:10:54 -0700

    update

--- exits.c
+++ exits.c
@@ -5,19 +5,18 @@
  * @dest: the destination string to be copied to
  * @src: the source string
  * @n: the amount of characters to be copied
- * Return: the concatenated string
+ * Return: the destination string
  */
-char *_strncpy(char *dest, const char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n - 1 && src[i] != '\0'; i++)
-        dest[i] = src[i];
+	for (i = 0; i < n - 1 && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
 
-    for (; i < n; i++)
-        dest[i] = '\0';
-
-    return dest;
+	return (dest);
 }
 
 /**
@@ -27,17 +26,17 @@ char *_strncpy(char *dest, const char *src, int n)
  * @n: the amount of bytes to be maximally used
  * Return: the concatenated string
  */
-char *_strncat(char *dest, const char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-    int dest_len = _strlen(dest);
-    int i;
+	int dest_len = _strlen(dest);
+	int i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++)
-        dest[dest_len + i] = src[i];
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
 
-    dest[dest_len + i] = '\0';
+	dest[dest_len + i] = '\0';
 
-    return dest;
+	return (dest);
 }
 
 /**
@@ -47,17 +46,17 @@ char *_strncat(char *dest, const char *src, int n)
  * Return: a pointer to the first occurrence of the character in the string
  *         or NULL if the character is not found
  */
-char *_strchr(const char *s, int c)
+char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-            return (char *)s;
-        s++;
-    }
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
 
-    if (c == '\0')
-        return (char *)s;
+	if (c == '\0')
+		return (s);
 
-    return NULL;
+	return (NULL);
 }
